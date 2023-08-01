a69c441263312552c8469bd76c88c162f758e360
Lau <laurabedoyacoronado@gmail.com>
2023-08-01 17:40:41 -0500

    commit ejercicio_1.py

---
+++ ejercicios/ejercicio_1.py
@@ -0,0 +1,3 @@
+nombre = input("Ingrese su nombre por favor: ")
+edad = input("Ingrese su edad por favor: ")
+print("Bienvenid@",nombre,"a nuestra pagina. Acabamos de confirmar que su edad es de", edad)
