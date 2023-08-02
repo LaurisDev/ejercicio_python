167b308ea3d9c897804315feedb47796b645ae4c
Lau <laurabedoyacoronado@gmail.com>
2023-08-01 19:22:09 -0500

    commit ejercicio_5.py

---
+++ ejercicios/ejercicio_5.py
@@ -0,0 +1,7 @@
+def conversion(grados):
+    celsius = (grados - 32)*5/9
+    return celsius
+
+grados_fahrenheit = int(input("Ingrese sus grados en Fahrenheit que quiere convertir: "))
+celsius = conversion(grados_fahrenheit)
+print("Sus grados convertidos a celsius es de: ",celsius)
