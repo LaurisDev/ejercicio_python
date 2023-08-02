947fb8fa4a7d457994fad25d68c201fa0020246d
Lau <laurabedoyacoronado@gmail.com>
2023-08-01 20:02:58 -0500

    commit ejercicio_13.py

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,14 @@
+numero_1 = int(input("Ingrese el primer numero entero que quiere operar: "))
+numero_2 = int(input("Ingrese el segundo numero entero que quiere operar: "))
+
+suma = (numero_1 + numero_2)
+print("La suma de sus numeros es de: ", suma)
+
+resta = (numero_1 - numero_2)
+print("La resta de sus numeros es de: ", resta)
+
+multiplicacion = (numero_1 * numero_2)
+print("La multiplicación de sus numeros es de: ", multiplicacion)
+
+division = (numero_1/numero_2)
+print("La división de sus numeros es de: ", division)
